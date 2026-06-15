ed15f39c064965726cb33c4702e4ebdcc3648475
Krishna Murari <yourgithubemail@example.com>
2026-06-15 13:15:43 +0530

    Exclude DataSourceAutoConfiguration for testing

--- eventsphere/src/main/java/com/college/events/EventsphereApplication.java
+++ eventsphere/src/main/java/com/college/events/EventsphereApplication.java
@@ -2,12 +2,11 @@ package com.college.events;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class EventsphereApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EventsphereApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EventsphereApplication.class, args);
+    }
 }
